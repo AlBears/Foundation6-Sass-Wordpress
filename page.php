e626e272fd7249727e05bf079915559f68f90ea8
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-19 14:51:04 +0300

    form contact us

--- page.php
+++ page.php
@@ -7,9 +7,17 @@
 
 <section class="content">
     <div class="row">
+      <?php if(is_page('contact-us')): ?>
+        <div class="large-6 columns large-centered">
+            <div class="row">
+              <?php the_content(); ?>
+            </div>
+          </div>
+      <?php else: ?>
         <div class="medium-12 columns">
           <?php the_content(); ?>
         </div>
+      <?php endif; ?>
     </div>
 </section>
 
